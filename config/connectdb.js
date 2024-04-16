caa1c532691c13b5d60c83cba1508bbb67b63ecb
Minh Nguyen <anhminh.dev@gmail.com>
2024-04-16 16:57:25 +0700

    Add pg, pg-hstore, and sequelize dependencies and connect db

---
+++ config/connectdb.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  `${process.env.DB_DIALECT}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  {
+    logging: false,
+    timezone: "+07:00",
+  }
+);
+
+const testDBConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+module.exports = testDBConnection;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const testDBConnection = require("./config/connectdb");
 
 const app = express();
 app.use(
@@ -10,6 +11,8 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+testDBConnection();
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
