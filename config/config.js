5552c26b7e28bf2d664114366430f4f54575821d
Minh Nguyen <anhminh.dev@gmail.com>
2024-04-17 11:53:45 +0700

    Add sequelize-cli as a devDependency in package.json to migrate model to db

---
+++ config/config.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    timezone: "+07:00",
+    port: process.env.DB_PORT,
+  },
+};
